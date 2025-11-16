179e1ac500912650da47d940deecdd2ffef44928
Arpita Raj <arpi59815@gmail.com>
2025-11-16 20:29:36 +0530

    Fixed API base URL for localhost and render

--- mini-project-react/src/api.js
+++ mini-project-react/src/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? import.meta.env.VITE_API_URL_PROD
-    : import.meta.env.VITE_API_URL_DEV,
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:5000"
+    : "https://image-finder-backend-lsf9.onrender.com",
   withCredentials: true,
 });
+
 
 export default api;
